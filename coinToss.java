78ec800b89091acfe41cbf1c0f7a863502837d69
Atsuki Kageyama <s1310160@u-aizu.ac.jp>
2024-06-28 11:59:06 +0900

    User name added

--- coinToss.java
+++ coinToss.java
@@ -1,11 +1,17 @@
 import java.util.Random;
-    
+import java.util.Scanner;
+
 class coinToss{
     private int N=3;
     private int countH=0, countT=0;
     private int[] in;
+    private String name;
 
     public coinToss(){
+      	Scanner sc = new Scanner(System.in);
+	System.out.println("Who are you?");
+	name = sc.next();
+	System.out.println("HEllo, " + name + "!");
 	Random random = new Random();
 	in = new int[N];
 	for(int i=0 ; i<N ; i++){
